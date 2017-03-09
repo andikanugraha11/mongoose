bc4ffee563563edffeb2ee1073ad9d18e18967ca
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-09 12:17:13 +0700

    + expressJS and setting router

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,37 @@
-var mongoose		= require('mongoose');
-var database		= "mongodb://localhost:27017/cobaMongoose";
+// var mongoose		= require('mongoose');
+// var database		= "mongodb://localhost:27017/cobaMongoose";
 
-mongoose.Promise 	= global.Promise;
-mongoose.connect(database);
+// mongoose.Promise 	= global.Promise;
+// mongoose.connect(database);
+var express			= require('express');
+var router			= express.Router();
 
 var User = require('../models/User');
 
 var dika = new User({
-	name		: 'dikadika',
-	username	: 'dikadika',
+	name		: 'qwertyuiop',
+	username	: 'qwertyuiop',
 	password	: 'password',
 	verified	: true
 });
 
-
-dika.save(function(err){
-	if(err){
-		console.log(err);
-	}else{
-		console.log('Data telah di tambah');
-	}
+router.get('/',function(req,res){
+	res.send('Root GET');
+	console.log('anda berada pada / userController');
 });
+
+router.get('/save', function(req, res){
+	console.log('anda sedang mengakses save document');
+	dika.save(function(err){
+		if(err){
+			console.log(err);
+		}else{
+			console.log('Data telah di tambah');
+		}
+	});
+});
+
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+var express			= require('express');
+var path 			= require('path');
 var mongoose		= require('mongoose');
-mongoose.Promise 	= global.Promise;
+
+var app				= express();
+
+//require controller
+var userController	= require("./controllers/user");
+
 var database		= "mongodb://localhost:27017/cobaMongoose";
+mongoose.Promise 	= global.Promise;
 mongoose.connect(database);
+
+app.use('/user', userController);
+
+app.get('/', function(req,res){
+	res.send('Hello World!');
+	console.log('anda berada pada directory root');
+});
+
+app.listen(3000, function(){
+	console.log('running at server 3000');
+})
